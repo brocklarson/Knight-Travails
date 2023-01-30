b5abc2f2aefd7a97c7e94e927b56e06e9910a6a1
brocklarson <brock.larson0725@gmail.com>
2023-01-30 16:44:53 -0700

    Validate user input and highlight path on chessboard

--- src/dom.js
+++ src/dom.js
@@ -4,9 +4,8 @@ const createChessboard = () => {
     for(let i = 0; i < 8; i++){
         for(let j = 0; j < 8; j++){        
             const square = document.createElement(`div`);
-            square.id = `${i}-${j}`;
+            square.id = `[${j},${7-i}]`;
             square.classList.add(`box`);
-            //square.innerText = `${i},${j}`;
             container.appendChild(square);
         }
     }
--- src/index.js
+++ src/index.js
@@ -9,16 +9,80 @@ const chessboard = new undirectedGraph();
 const button = document.getElementById(`searchBtn`);
 const startInput = document.getElementById(`startInput`);
 const endInput = document.getElementById(`endInput`);
+const squares = document.querySelectorAll(`.box`)
 
-function validateInput(value){
-    //return undefined if the value is empty
-    //make sure the input follows format
-}
+const startError = document.querySelector(`#startInput + span.error`);
+const endError = document.querySelector(`#endInput + span.error`);
 
 button.addEventListener(`click`, handleClick);
+
+function showError(error){
+    error.innerText = `Invalid input`;
+    error.classList.add(`active`);
+}
+
+function removeError(error){
+    error.textContent = ``;
+    error.classList.remove(`active`);
+}
+
+function validateInput(value, error){
+    value = value.toLowerCase();
+
+    if (value == "") {
+        showError(error);
+        return null; 
+    }
+    else if (value.length != 2) {
+        showError(error);
+        return null; 
+    }
+    else if (value.charCodeAt(0) < 97 || value.charCodeAt(0) > 104) {
+        showError(error);
+        return null; 
+    }
+    else if (value.charCodeAt(1) < 49 || value.charCodeAt(1) > 56) {
+        showError(error);
+        return null; 
+    }
+
+    removeError(error);
+    return value;   
+}
+
+const delay = ms => new Promise(res => {
+    console.log('waiting');
+    setTimeout(res, ms)
+});
+
+function highlightSquares(path){
+    let i = 0; 
+    path.forEach(value =>{
+        const square = Array.from(squares).find(square =>{
+            if(square.id == value) {
+                i += 1;
+                return true;
+            }
+        });
+        square.style.backgroundColor = "red";
+        square.innerText = i;
+
+    });
+}
+
+function resetSquares(){
+    Array.from(squares).forEach(square => {
+        square.style.backgroundColor = "";
+        square.innerText = "";
+    })
+}
+
 function handleClick(){
-    const start = validateInput(startInput.value);
-    const end = validateInput(endInput.value);
+    resetSquares();
+    const start = validateInput(startInput.value, startError);
+    const end = validateInput(endInput.value, endError);
+    if (start == null || end == null) return;
     const path = chessboard.findShortestPath(start, end);
+    highlightSquares(path)
     console.log(chessboard.convertOutput(path));
 }
